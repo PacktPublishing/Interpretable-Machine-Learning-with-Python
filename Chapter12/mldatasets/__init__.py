f7e972f2a2deaf60a3afedcc737b217d67d9788d
Sergio Masís <smasis@gmail.com>
2020-12-31 11:57:18 -0600

    Chapter 12 + Correction Chapter 5

---
+++ Chapter12/mldatasets/__init__.py
@@ -0,0 +1,16 @@
+import sys
+import warnings
+import os
+from pathlib import Path
+
+if (sys.version_info < (3, 0)):
+    warnings.warn("mldatasets only supports Python 3 (not 2)!")
+
+__version__ = '0.01.10'
+
+from mldatasets.config import init, load
+from mldatasets.common import runcmd, make_dummies_with_limits, make_dummies_from_dict, evaluate_multiclass_mdl, evaluate_reg_mdl, evaluate_class_mdl, plot_3dim_decomposition, encode_classification_error_vector, describe_cf_instance, create_decision_plot, plot_data_vs_ice, img_np_from_fig, compare_img_pred_viz, heatmap_overlay, find_closest_datapoint_idx, approx_predict_ts, compare_confusion_matrices, discretize, plot_prob_progression, plot_prob_contour_map
+from mldatasets.sources.kaggle import Kaggle
+from mldatasets.sources.url import URL
+
+init(os.path.join(Path().parent.absolute(), 'data'))
